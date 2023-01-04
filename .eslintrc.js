98285f3ee6ea5b37e0071f36dad1fc617c617619
Vadym Baranov <vadym.baranov@hotmail.com>
2023-01-04 20:04:24 +0200

    Added prettier, editorconfig, ignore config files and scripts for prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'airbnb-typescript',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
