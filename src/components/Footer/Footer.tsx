0ad76b65e0db9d17af81d139370a91f6add7db3c
Andrey Olin <crazy106ras@gmail.com>
2023-01-05 16:05:41 +0200

    footer added (#8)

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Logo } from '../Logo';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <Logo />
+
+      <div className="footer__nav">
+        <a
+          href="/github"
+          className="footer__nav__link"
+        >
+          github
+        </a>
+
+        <a
+          href="/contacts"
+          className="footer__nav__link"
+        >
+          contacts
+        </a>
+
+        <a
+          href="/rights"
+          className="footer__nav__link"
+        >
+          rights
+        </a>
+      </div>
+
+      <div className="footer__topscroll">
+        Back to top
+
+        <button
+          type="button"
+          className="footer__topscroll__button"
+          aria-label="Save"
+          onClick={() => window.scrollTo({ top: 0 })}
+        />
+      </div>
+    </footer>
+  );
+};
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Logo: React.FC = () => {
+  return (
+    <div className="logo" />
+  );
+};
