ab0f366f92b50e20c827d8dd9e15a81f6f0a69d2
Andrii Bassarab <andrijbassarab@gmail.com>
2023-01-06 15:05:20 +0200

    adding header

--- src/components/pages/AccessoriesPage.tsx
+++ src/components/pages/AccessoriesPage.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import '../../styles/page-title.scss';
-import { Header } from '../Header/Header';
 
 export const AccessoriesPage = () => (
-  <>
-    <Header />
-    <h1 className="page-title">Accessories Page</h1>
-  </>
+  <h1 className="page-title">Accessories Page</h1>
 );
