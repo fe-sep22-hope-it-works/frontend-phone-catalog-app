63199a62aa3b9aa07a4bb33f841b36acae345293
Andrii Bassarab <110697913+andrii-bassarab@users.noreply.github.com>
2023-01-06 11:37:37 +0300

    first try (#22)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,75 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Logo } from '../Logo';
 import { NavItem } from './NavItem';
+import '../../styles/Header.scss';
 
 export const Header = () => {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  // const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const resize = () => {
+    setInnerWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', resize.bind(this));
+    // setInnerWidth(window.innerWidth);
+  }, []);
+
   return (
     <header className="header">
-      <Link to="/">
-        <img src="" alt="logo" />
-      </Link>
-      <nav
-        className="nav"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div className="nav__container">
-          <NavItem path="/" text="Home Page" />
-          <br />
-          <NavItem path="/phones" text="Phones" />
-          <br />
-          <NavItem path="/tablets" text="Tablets" />
-          <br />
-          <NavItem path="/accessories" text="Accessories" />
-        </div>
-      </nav>
-      <div className="my-puchases">
-        <nav>
-          <NavItem path="/favourites" text="favourites" />
-          <br />
-          <NavItem path="/purchases" text="purchases" />
-        </nav>
-      </div>
+      {innerWidth > 639
+        ? (
+          <div className="header__container">
+            <div className="header__nav">
+              <Link to="/">
+                <Logo />
+              </Link>
+              <nav
+                className="nav"
+                role="navigation"
+                aria-label="main navigation"
+              >
+                <NavItem path="/" text="Home" />
+                <NavItem path="/phones" text="Phones" />
+                <NavItem path="/tablets" text="Tablets" />
+                <NavItem path="/accessories" text="Accessories" />
+              </nav>
+            </div>
+            <div className="my-puchases">
+              <nav className="my-puchases__nav">
+                <div className="my-puchases__icon">
+                  <NavItem
+                    path="/favourites"
+                    text=""
+                    classname="my-puchases__img my-puchases__img--favourites"
+                  />
+                </div>
+                <div className="my-puchases__icon">
+                  <NavItem
+                    path="/purchases"
+                    text=""
+                    classname="my-puchases__img my-puchases__img--purchases"
+                  />
+                </div>
+              </nav>
+            </div>
+          </div>
+        )
+        : (
+          <div className="header__container">
+            <Link to="/">
+              <Logo />
+            </Link>
+
+            <div className="my-puchases__icon">
+              <button className="my-puchases__img my-puchases__img--menu" />
+            </div>
+          </div>
+        )}
     </header>
   );
 };
