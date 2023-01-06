1e21585553b3bf9ae0e2b1f72589ecad77cb1429
Andrii Bassarab <andrijbassarab@gmail.com>
2023-01-06 10:25:54 +0200

    first try

--- src/components/Header/NavItem.tsx
+++ src/components/Header/NavItem.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
+import '../../styles/Header.scss';
 
 type Props = {
   path: string,
   text: string,
+  classname?: string,
 };
 
-export const NavItem: React.FC<Props> = ({ path, text }) => (
+export const NavItem: React.FC<Props> = ({ path, text, classname }) => (
   <NavLink
-    className={({ isActive }) => classNames({
-      'nav__active-link': isActive,
+    className={({ isActive }) => classNames(classname, {
+      nav__link: !classname,
+      'nav__link--active': isActive && !classname,
+      'my-puchases__img--active': isActive && classname,
     })}
     to={path}
   >
