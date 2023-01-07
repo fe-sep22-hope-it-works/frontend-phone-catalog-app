b40783fc3aa0347d8c2891d46506ef6a7bccaa45
Vadym Baranov <vadym.baranov@hotmail.com>
2023-01-07 12:16:30 +0200

    Added new favicon, bulma(CDN), started work on 404 page

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -1,6 +1,35 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/page-title.scss';
 
-export const NotFoundPage = () => (
-  <h1 className="page-title">Page not found</h1>
-);
+export const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="not-found__container">
+      <div className="not-found__message message">
+        <p className="message__main">
+          There are no phones here. You should
+          <button className="button is-danger" onClick={() => navigate(-1)}>
+            Go back
+          </button>
+          or
+          <button className="button is-link" onClick={() => navigate('/')}>
+            Go home
+          </button>
+          to get out of this sticky situation!
+        </p>
+
+        <a
+          // eslint-disable-next-line max-len
+          href="https://www.freepik.com/free-vector/funny-error-404-background-design_1161579.htm#query=404%20background&position=35&from_view=keyword"
+          className="message__copyright"
+        >
+          Image by bamdewanto on Freepik
+        </a>
+      </div>
+
+    </div>
+  );
+};
