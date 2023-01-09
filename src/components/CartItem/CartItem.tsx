2c1fe98f1aa570942f5daac632f09da8d4a0625d
Andrey Olin <crazy106ras@gmail.com>
2023-01-09 22:52:25 +0200

    Cart calculating (#36)
    * firts commit
    
    * minor fixes
    
    * calculating cart
    
    * cart calculating logic implemented
    
    * minor fix
    
    * Update Cart.tsx
    
    * Update Cart.tsx
    
    * Update Cart.tsx
    
    * Update Card.tsx

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import img from '../../img/card-images/iphone.svg';
+import { Phone } from '../../types/Phone';
 
-export const CartItem: React.FC = () => {
+interface Props {
+  product: Phone,
+  changeProductQuantity: (productToChange: Phone, newQuantity: number) => void;
+  removeItem: (idToRemove: number) => void;
+}
+
+export const CartItem: React.FC<Props> = ({
+  product, changeProductQuantity, removeItem,
+}) => {
+  const [
+    productQuantity, setProductQuantity,
+  ] = useState(product.quantity ? product.quantity : 1);
+
+  useEffect(() => {
+    changeProductQuantity(product, productQuantity);
+  }, [productQuantity]);
+
   return (
-    <div className="cart__item">
+    <>
       <div className="cart__item__title">
         <button
           type="button"
           className="cart__item__title__button"
           aria-label="Save"
+          onClick={() => removeItem(product.id)}
         />
 
         <img
@@ -18,7 +36,7 @@ export const CartItem: React.FC = () => {
         />
 
         <div className="cart__item__title__text">
-          Title
+          {product.name}
         </div>
       </div>
 
@@ -31,12 +49,16 @@ export const CartItem: React.FC = () => {
               cart__item__price__quantity__button
               cart__item__price__quantity__button__decrease
             "
+            onClick={() => setProductQuantity(prevQty => prevQty - 1)}
+            disabled={productQuantity === 1}
           >
             -
           </button>
-
-          <div className="cart__item__price__quantity__text">1</div>
-
+          <div
+            className="cart__item__price__quantity__text"
+          >
+            {productQuantity}
+          </div>
           <button
             type="button"
             aria-label="Save"
@@ -44,15 +66,19 @@ export const CartItem: React.FC = () => {
               cart__item__price__quantity__button
               cart__item__price__quantity__button__increase
             "
+            onClick={() => setProductQuantity(
+              (prevQty: number) => prevQty + 1,
+            )}
           >
             +
           </button>
         </div>
 
         <div className="cart__item__price__text">
-          $999
+          $
+          {product.price}
         </div>
       </div>
-    </div>
+    </>
   );
 };
--- src/components/pages/AccessoriesPage.tsx
+++ src/components/pages/AccessoriesPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../../styles/page-title.scss';
+import { ProductsList } from '../ProductsList';
 
 export const AccessoriesPage = () => (
   <>
     <h1 className="page-title">Accessories Page</h1>
+    <ProductsList />
   </>
 
 );
