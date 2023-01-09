f5d024b753e5957788a075ae3512161d2251eace
Andrii Bassarab <andrijbassarab@gmail.com>
2023-01-09 17:03:11 +0200

    linter

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,11 @@ import { RoutesList } from './components/RoutesList';
 export function App() {
   const [burgerMenuStatus, isBurgerMenu] = useState(false);
 
-  // const getPhonesFromServer = async () => {
-  //   const phonesFromServer = await getAllPhones(SortBy.ALPHABETCALLY);
   //   const getImageFromServer = async () => {
   //     const imageFromServer = await getPhoneImage(34, 2);
 
   //     return imageFromServer?.slice(22);
   //   };
-  // };
 
   // useEffect(() => {
   //   getImageFromServer();
