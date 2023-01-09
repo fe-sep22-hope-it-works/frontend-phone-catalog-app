d4be3b1960e5b925406acdc814d70a4356a76a6d
Vadym Baranov <vadym.baranov@hotmail.com>
2023-01-09 13:45:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 // import { Card } from './components/Card';
-import { getPhoneImage } from './components/api/phones';
+// import { getPhoneImage, getAllPhones } from './components/api/phones';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header/Header';
 import { RoutesList } from './components/RoutesList';
@@ -10,18 +10,18 @@ import { RoutesList } from './components/RoutesList';
 export function App() {
   const [burgerMenuStatus, isBurgerMenu] = useState(false);
 
-  const getPhonesFromServer = async () => {
-    const phonesFromServer = await getAllPhones(SortBy.ALPHABETCALLY);
-    const getImageFromServer = async () => {
-      const imageFromServer = await getPhoneImage(34, 2);
+  // const getPhonesFromServer = async () => {
+  //   const phonesFromServer = await getAllPhones(SortBy.ALPHABETCALLY);
+  //   const getImageFromServer = async () => {
+  //     const imageFromServer = await getPhoneImage(34, 2);
 
-      return imageFromServer?.slice(22);
-    };
-  };
+  //     return imageFromServer?.slice(22);
+  //   };
+  // };
 
-  useEffect(() => {
-    getImageFromServer();
-  }, []);
+  // useEffect(() => {
+  //   getImageFromServer();
+  // }, []);
 
   return (
     <div className="app">
