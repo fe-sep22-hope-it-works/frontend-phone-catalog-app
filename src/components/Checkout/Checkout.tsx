9574a487dda53944d3dfa932f8573e742ee2e1e9
Vadym Baranov <vadym.baranov@hotmail.com>
2023-01-09 22:36:10 +0200

    Checkout page completed, timer added

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+/* eslint-disable consistent-return */
+import React, { useState, useEffect } from 'react';
+import { redirect } from 'react-router-dom';
 
 export const Checkout: React.FC = () => {
+  const [seconds, setSeconds] = useState(10);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds(seconds - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  });
+
   return (
     <div className="checkout__container">
-      <div className="checkout_message message">
+      <div className="checkout__message message">
+        <div className="message__img" />
+
         <div className="message__main">
-          <h1 className="message__main-thanks">THANK YOU!</h1>
+          <h1 className="message__main-thanks">THANK YOU FOR YOUR ORDER!</h1>
         </div>
 
         <div className="message__confirmation">
-          <h2 className="confirmation">
-            A confirmation has been sent to your email
-          </h2>
+          <>
+            <h2 className="confirmation">
+              A confirmation with your order details has been sent to your
+              email.
+            </h2>
+            {seconds === 0 ? (
+              redirect('/home')
+            ) : (
+              <h3 className="redirect">
+                You will be redirected to Home page in &nbsp;
+                {seconds}
+                &nbsp; seconds.
+              </h3>
+            )}
+          </>
         </div>
       </div>
     </div>
