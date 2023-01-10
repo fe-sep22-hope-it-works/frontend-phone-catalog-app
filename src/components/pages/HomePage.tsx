32e33c82e98cfd284a858c6289b8b0f5c9bfdc8d
Vadym Baranov <vadym.baranov@hotmail.com>
2023-01-10 22:15:12 +0200

    Phones loaded, started working on styles and carousel

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../styles/page-title.scss';
-import { HotPrices } from '../HotPrices';
 import { NewPhones } from '../NewPhones';
+import { HotPrices } from '../HotPrices';
 import { Slider } from '../Slider/Slider';
 
 export const HomePage = () => {
