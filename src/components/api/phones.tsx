5cb198307943f678764001f3f499317eaeb6e34f
Andrii Bassarab <andrijbassarab@gmail.com>
2023-01-11 10:18:24 +0200

    fix fetch

--- src/components/api/phones.tsx
+++ src/components/api/phones.tsx
@@ -1,6 +1,6 @@
 import { Phone } from '../../types/Phone';
 import { SortBy } from '../../types/SortBy';
-import { getImages, getPhones } from '../../utils/fetch';
+import { getPhones } from '../../utils/fetch';
 
 export const getAllPhones = (sortBy: SortBy = SortBy.ALPHABETCALLY) => {
   return getPhones<Phone[]>(`/phones?sortedby=${sortBy}&page=1&phonesQuantity=71`);
@@ -10,6 +10,6 @@ export const getPhoneById = (id: number) => {
   return getPhones<Phone>(`/phones/${id}`);
 };
 
-export const getPhoneImage = (id: number, numberOfImage: number) => {
-  return getImages(`/public/${id}/0${numberOfImage}`);
+export const getPhoneImage = (id: number) => {
+  return getPhones<string[]>(`/public/img/${id}`);
 };
--- src/components/pages/TabletsPage.tsx
+++ src/components/pages/TabletsPage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import '../../styles/page-title.scss';
-import { ProductsList } from '../ProductsList';
 
 export const TabletsPage = () => (
   <>
     <h1 className="page-title">Tablets Page</h1>
-    <ProductsList />
   </>
 );
