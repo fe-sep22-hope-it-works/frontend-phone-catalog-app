bbb4cfc080d31803e7ae37e6cbed977dd26c330e
Vadym Baranov <vadym.baranov@hotmail.com>
2023-01-13 10:53:27 +0200

    404 page fixed

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -28,16 +28,6 @@ export const NotFoundPage: React.FC = () => {
             &nbsp;to get out of this sticky situation!
           </p>
         </div>
-
-        <div className="message__copyright">
-          <a
-            // eslint-disable-next-line max-len
-            href="https://www.freepik.com/free-vector/funny-error-404-background-design_1161579.htm#query=404%20background&position=35&from_view=keyword"
-            className="copyright"
-          >
-            Image by bamdewanto on Freepik
-          </a>
-        </div>
       </div>
     </div>
   );
